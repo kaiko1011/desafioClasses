24fd3918e883512e6625f6b9f6359e5b058e576c
kaiko1011 <106026827+kaiko1011@users.noreply.github.com>
2024-01-21 00:43:20 -0300

    Add files via upload

---
+++ desafioClass.js
@@ -0,0 +1,39 @@
+class Heroi{
+    constructor(nome,idade,tipo){
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+    atacar() {
+        let ataque = " "
+        switch(this.tipo){
+           case "guerreiro" :
+              ataque = "espada"
+            break;
+            case "mago" :
+                ataque = "magia"
+            break;
+            case "monge" :
+                ataque = "artes marciais"
+            break;
+            case "ninja" :
+                ataque = "shuriken"
+            break;
+            default :
+                ataque = "as própias mãos"
+        }
+
+        console.log(`O ${this.tipo} atacou usando ${ataque}`)
+    }
+}
+
+const Mago = new Heroi("Merlin", 101 , "mago" )
+const Guerreiro = new Heroi("Goldar",200,"guerreiro")
+const Monge = new Heroi("Matusalem", 110 , "monge")
+const Ninja = new Heroi("Brece Lee", 90 , "ninja")
+const Boxeador = new Heroi("Brece Lee", 90 , "boxeador")
+Mago.atacar()
+Guerreiro.atacar()
+Monge.atacar()
+Ninja.atacar()
+Boxeador.atacar()
